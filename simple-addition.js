4d181c539c388ab010c610c19cfb937e6b778548
Razvan <razvan.horeanga@gmail.com>
2016-09-07 07:40:37 -0500

    added solution to simple addition

---
+++ simple-addition.js
@@ -0,0 +1,12 @@
+function SimpleAdding(num) { 
+    var sum = 0;
+  for (var i=1; i<=num; i++) {
+      sum = sum + i;
+  }
+         return sum;
+}
+   
+// keep this function call here 
+SimpleAdding(readline());                            
+
+
